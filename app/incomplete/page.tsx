33a3fb0ac1bf5efff7ef6475b8edf27488712068
Swetna <48342893+Swetna@users.noreply.github.com>
2025-06-02 01:01:49 -0400

    Add files via upload

---
+++ app/incomplete/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from "react";
+
+function page(){
+
+    return <div>
+        todo
+    </div>
+}
+
+
+export default page;
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import Tasks from "./Components/Tasks/tasks";
+
+export default function Home() {
+  return <Tasks/> ;
+}
+
+
